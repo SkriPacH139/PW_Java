631c78d43fdcdad04c9625dca1112378fc5741e8
SkriPacH139 <79088983+SkriPacH139@users.noreply.github.com>
2024-09-16 20:12:01 +0300

    Delete PW/PW_03/PW_03 directory

--- PW/PW_03/PW_03/src/PaymentSystem.java
+++
@@ -1,4 +0,0 @@
-public interface PaymentSystem {
-    void pay(double amount);
-    void refund(double amount);
-}
--- PW/PW_03/PW_03/src/_Animal.java
+++
@@ -1,9 +0,0 @@
-public class _Animal {
-    private int count;
-    private UniqueID id = new UniqueID(5, "_animal: ", count);
-
-    public void move(){
-        System.out.println("Animal move");
-        count++;
-    }
-}
